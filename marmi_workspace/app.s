dd9047c952250746dd157b7425fe03b732568873
Tomas's Home <tomasmarmay21@gmail.com>
2022-05-30 20:30:02 -0300

    fondo listo

--- marmi_workspace/app.s
+++ marmi_workspace/app.s
@@ -6,8 +6,12 @@
 
 .equ HALF_SCREEN_HEIGH, 240 // Dimencion Cielo
 .equ SKYE_COLOR_I, 0x88FF // Color inical del cielo
+.equ INCREMENT_COLOR, 0x80000 // Incremento para degrades
 .equ Q_LINE, 10 // Cantidad de lineas del mismo color
-.equ SEA_COLOR, 0x44FF // Color mar
+
+.equ BASE_COLOR, 0xFF00 // Color base
+.equ SEA_COLOR_l, 0x00FF // Color mar
+
 .globl main
 
 main:
@@ -15,7 +19,7 @@ main:
  	mov x20, x0	// Save framebuffer base address to x20	
 	//---------------- CODE HERE ------------------------------------
 	
-	movz x10, 0xFF00, lsl 16
+	movz x10, BASE_COLOR, lsl 16
 	movk x10, SKYE_COLOR_I, lsl 00
 
 	mov x11, Q_LINE // Cantidad de renglones con el mismo color
@@ -34,9 +38,11 @@ loop0:
 	cbnz x1,loop0	   // If not end row jump
 
 	/*Degrades*/
+	cmp x12,2
+	b.ne decrement_y
 	sub x11,x11,1 
 	cbnz x11, decrement_y // Compara si termino de pintar el bloque del mismo color
-	add x9, x10, 0x80000 // Calcular nuevo color
+	add x9, x10, INCREMENT_COLOR // Calcular nuevo color
 	mov x10, x9 // Seteo de nuevo color
 	mov x11,Q_LINE // Reinicio de contador de renglones
 
@@ -45,6 +51,16 @@ loop0:
 	cbnz x2,loop1	   // if not last row, jump
 	/*Termina de pintar el cielo*/
 	
+	sub x12, x12, 1
+	cmp x12, 1
+	b.ne InfLoop
+	mov x2, HALF_SCREEN_HEIGH
+	
+	movz x10, BASE_COLOR, lsl 16
+	movk x10, SEA_COLOR_l, lsl 00
+	
+	b loop1
+
 //---------------------------------------------------------------
 // Infinite Loop 
 InfLoop: 
