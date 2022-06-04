dce20c19268a58de93ec4934f0e4845dbf1964ff
Tomas's Surface <tomasmarmay21@gmail.com>
2022-06-04 00:56:05 -0300

    casi erminada la img

--- main_workspace/app.s
+++ main_workspace/app.s
@@ -9,10 +9,14 @@
 .equ Q_LINE, 10 // Cantidad de lineas del mismo color
 
 .equ BASE_COLOR, 0xFF00 // Color base
-.equ SEA_COLOR_l, 0x00FF // Color mar
+.equ SEA_COLOR_l, 0x008F // Color mar
 
-
-
+.equ DIM_WAVES_Y, 250
+.equ DIM_WAVES_X, 640
+.equ POS_WAVES, 0
+.equ WAVES_COLOR, 0x00FF
+.equ DIST_WAVES, 3000
+.equ RADIO_WAVES, 150
 .globl main
 
 main:
@@ -55,7 +59,7 @@ loop0:
 	
 	sub x12, x12, 1 // Primera mitad pintada 
 	cmp x12, 1 
-	b.ne InfLoop
+	b.ne sea_effects
 
 	mov x2, HALF_SCREEN_HEIGH // Pinte segunda mitad
 	
@@ -63,16 +67,231 @@ loop0:
 	movk x10, SEA_COLOR_l, lsl 00 // Seteo color del mar
 	b loop1
 
+sea_effects:
+	// Dir base del Circulo
+	movz x3, DIM_WAVES_Y			
+	movz x4, DIM_WAVES_X
+	mul x3, x3, x4
+	add x3, x3, POS_WAVES
+	lsl x3, x3, 2
+	add x3,x20,x3	// Lo mandé a el centro de la pantalla
+
+	mov x7, x3 // guardo la pos inicial del circulo
+	mov x8, RADIO_WAVES // radio mov x22, del circulo
+	mov x9, x8
+	//Pintar Circulo	
+
+	pintarLinea:
+
+	movz x6, WAVES_COLOR, lsl 00
+	
+	stur w6, [x3] // Pinta la ola 
+	add x3, x3, 4 
+	sub x8, x8, 1 
+	cbnz x8, pintarLinea // Movimiento en eje x
+
+	sub x9, x9, 1 // Movimiento en eje y
+	mov x8, x9
+
+	add x3, x3, DIST_WAVES // Espacio entre linea 
+	cbnz x9, pintarLinea 
+
+	
+//tomi barco
+
+	movz x12, 320		// cordenada Y de 160 hasta 420
+	movz x13, 640		// 
+	mul x12, x12, x13	// calcula el centro del barco
+	add x12, x12, 200	// cordenada X de 
+	lsl x12, x12, 2		//
+	add x12,x20,x12     // 
 
 
-
-.include "barco.s"
-b lop00
-
+	bl make_boat
 
 //---------------------------------------------------------------
 // Infinite Loop 
+
 InfLoop: 
 	b InfLoop
+
+
+make_boat:
+
+	movz x11, 0xFF8B, lsl 16
+	movk x11, 0x4513, lsl 00
+
+	b base							//hacer base del barco
+endbase:
+	sub x13, x12, 300
+	mov x29, 150           // altura mastil
+	mov x28, 70				// "mitad" altura vela
+	b vela						//hacer la vela
+retur_boat:
+
+	add x13, x12, 200
+	mov x29, 100           		// altura mastil
+	mov x28, 50					// "mitad" altura vela
+	bl vela						//hacer la vela
+
+
+	br x30
+
+base : 
+	mov x4, x12	  		//centro del barco
+	sub x5, x4 ,600		//esquina izquierda del barco
+	add x7,x4, 600      //esquina derecha del barco
+	
+	mov x19, 20
+	
+	mov x16,640
+	lsl x16,x16,2    //calc aux avanzar sig linea
+
+
+	mov x8, x5
+	mov x6, 1
+	lineas:
+		stur xzr,[x8]
+		add x8,x8,4
+		subs xzr,x8,x7
+		bne lineas
+
+	nexttable:
+
+
+		add x5, x5 ,12
+		sub x7, x7, 12
+
+		mov x6, 3
+	first:
+	for:			   //pintar la tabla
+		
+		add x5,x5,x16
+		stur xzr,[x5]
+		mov x8, x5
+		add x8,x8,4
+		stur xzr,[x8]
+		add x8,x8,4	
+		add x7,x7,x16
+		mov x9,x7
+		stur xzr,[x9]
+		add x9,x9,4
+		stur xzr,[x9]
+		add x9,x9,4
+	aux:
+		stur w11,[x8]
+		add x8,x8,4
+		subs xzr,x9,x8
+		bne aux
+
+		
+		sub x6, x6,1
+		cbnz x6 , for
+
+		sub x19,x19,1
+		cbnz x19,nexttable
+
+		add x5,x5,x16
+		add x7,x7,x16
+		lineas2:
+		stur xzr,[x5]
+		add x5,x5,4
+		subs xzr,x5,x7
+		bne lineas2
+
+	b endbase
+
+
+vela: 
+
+	b mastil
+back:
+
+	b triang
+
+	br x30
+
+
+triang:
+	mov x21, x7 //centro
+	mov x19, x7
+	mov x22, x28 //altura triang
+	mov x8, 4 //incremento lados
+	b bucle 
+	
+	
+
+bucle:
+	movz x13, 0xFFFF , lsl 00
+	movk x13, 0xFFFF , lsl 16
+
+	mov x16,640
+	lsl x16,x16,2 //calc aux
+	add x21,x21,x16
+	
+	mov x18,x21 //prox linea
+	mov x19,x21
+
+	add x19,x19,x8 //moverme der
+
+	add x8,x8,4 //incremento lados
+
+aux_2:
+
+	stur w13,[x18] // colorer
+	add x18,x18,4
+	subs xzr, x18,x19
+	bne aux_2 
+
+	sub x22,x22,1 //decremento altura triang
+	cbnz x22, bucle
+	
+	sub x22, x28, 20 
+bucle2:
+	
+	add x21,x21,x16
+	mov x18,x21 //prox linea
+	mov x19,x21
+	add x19,x19,x8 //moverme der
+	sub x8,x8,4
+	
+
+aux_3:	
+	stur w13,[x18] // colorer
+	add x18,x18,4
+	subs xzr, x18,x19
+	bne aux_3 
+
+	sub x22,x22,1 //decremento altura triang
+	cbnz x22,bucle2 
+	
+	
+	b retur_boat
+
+
+mastil:
+
+	mov x4, x13   		// esquina inf izquierda del bote
+	mov x5, 10
+	
+	mov x16,640
+	lsl x16,x16,2 
+		
+	top:
+		add x4,x4 ,4
+		mov x7,x4			
+		mov x6,x29  			//largo del mastil 
+		follow:
+
+		stur w11,[x7]	
+		sub x7,x7,x16
+		sub x6,x6,1
+		cbnz x6, follow
+	
+		sub x5,x5,1
+	cbnz x5, top
+	add x7,x7,4
+
+	b back		// al final x7 debe guardar la esquina sup derecha +1 bit
 
 
