1fa6631760120a61e9cb2806fa914f4812b136cd
timoh <tomashubman47@gmail.com>
2022-06-05 19:05:00 -0300

    pseudo animacion

--- tomi_workspace/testing/app.s
+++ tomi_workspace/testing/app.s
@@ -33,6 +33,166 @@ main:
 	mov x12, 2 // Contador para mitad de la imagen
 	mov x2, HALF_SCREEN_HEIGH         // Y Size cielo
 
+
+
+
+	bl fondo
+	
+//tomi barco
+
+	movz x15, 0xFFCD , lsl 16	// color de la base del barco
+	movk x15, 0x853F , lsl 0	//
+	
+
+	movz x12, 300		// cordenada Y de 160 hasta 420
+	movz x13, 640		// 
+	mul x12, x12, x13	// calcula el centro del barco
+	add x12, x12, 200	// cordenada X de 
+	lsl x12, x12, 2		//
+	add x12,x20,x12     // 
+
+	movz x14, 0xFF , lsl 16		//color de las velas
+	movk x14, 0xFFFF , lsl 0	//
+
+
+	bl make_boat
+
+	movz x15, 0xFF8B , lsl 16	// color de la base del barco
+	movk x15, 0x4513 , lsl 0	//	
+
+
+	movz x12, 380		// cordenada Y de 160 hasta 420
+	movz x13, 640		// 
+	mul x12, x12, x13	// calcula el centro del barco
+	add x12, x12, 460	// cordenada X de 
+	lsl x12, x12, 2		//
+	add x12,x20,x12     // 
+
+	movz x14, 0x0 , lsl 16	// color de las velas
+	movk x14, 0x0 , lsl 0	//
+	bl make_boat
+
+// primer frame
+	movz x17, 0x1FF, lsl 16 // "tiempo" del delay
+
+bl delay
+
+
+	movz x10, BASE_COLOR, lsl 16
+	movk x10, SKYE_COLOR_I, lsl 00
+
+	mov x11, Q_LINE // Cantidad de renglones con el mismo color
+	mov x9, SKYE_COLOR_I // Seteo variable de color
+	mov x12, 2 // Contador para mitad de la imagen
+	mov x2, HALF_SCREEN_HEIGH         // Y Size cielo
+
+	bl fondo
+
+	movz x15, 0xFFCD , lsl 16	// color de la base del barco
+	movk x15, 0x853F , lsl 0	//
+	
+
+	movz x12, 300		// cordenada Y de 160 hasta 420
+	movz x13, 640		// 
+	mul x12, x12, x13	// calcula el centro del barco
+	add x12, x12, 210	// cordenada X de 
+	lsl x12, x12, 2		//
+	add x12,x20,x12     // 
+	
+	movz x14, 0xFFFF , lsl 16		//color de las velas
+	movk x14, 0xFFFF , lsl 0	//
+
+
+	bl make_boat
+
+	movz x15, 0xFF8B , lsl 16	// color de la base del barco
+	movk x15, 0x4513 , lsl 0	//	
+
+
+	movz x12, 380		// cordenada Y de 160 hasta 420
+	movz x13, 640		// 
+	mul x12, x12, x13	// calcula el centro del barco
+	add x12, x12, 470	// cordenada X de 
+	lsl x12, x12, 2		//
+	add x12,x20,x12     // 
+
+	movz x14, 0x0 , lsl 16	// color de las velas
+	movk x14, 0x0 , lsl 0	//
+	bl make_boat
+
+// second frame
+
+	movz x17, 0x1FF, lsl 16 // "tiempo" del delay
+
+	bl delay
+
+
+	movz x10, BASE_COLOR, lsl 16
+	movk x10, SKYE_COLOR_I, lsl 00
+
+	mov x11, Q_LINE // Cantidad de renglones con el mismo color
+	mov x9, SKYE_COLOR_I // Seteo variable de color
+	mov x12, 2 // Contador para mitad de la imagen
+	mov x2, HALF_SCREEN_HEIGH         // Y Size cielo
+
+	bl fondo
+
+	movz x15, 0xFFCD , lsl 16	// color de la base del barco
+	movk x15, 0x853F , lsl 0	//
+	
+
+	movz x12, 300		// cordenada Y de 160 hasta 420
+	movz x13, 640		// 
+	mul x12, x12, x13	// calcula el centro del barco
+	add x12, x12, 220	// cordenada X de 
+	lsl x12, x12, 2		//
+	add x12,x20,x12     // 
+
+	movz x14, 0xFFFF , lsl 16		//color de las velas
+	movk x14, 0xFFFF , lsl 0	//
+
+
+	bl make_boat
+
+	movz x15, 0xFF8B , lsl 16	// color de la base del barco
+	movk x15, 0x4513 , lsl 0	//	
+
+
+	movz x12, 380		// cordenada Y de 160 hasta 420
+	movz x13, 640		// 
+	mul x12, x12, x13	// calcula el centro del barco
+	add x12, x12, 490	// cordenada X de 
+	lsl x12, x12, 2		//
+	add x12,x20,x12     // 
+
+	movz x14, 0x0 , lsl 16	// color de las velas
+	movk x14, 0x0 , lsl 0	//
+	bl make_boat
+
+
+
+//---------------------------------------------------------------
+// Infinite Loop 
+
+InfLoop: 
+	b InfLoop
+
+
+// fun delay
+	
+delay:
+	delay1:
+	sub x17,x17,1
+	cbnz x17 , delay1
+	br x30
+
+
+// hacer fondo principio
+
+fondo:
+
+mov x0, x20
+
 loop1:
 	mov x1, SCREEN_WIDTH         // X Size
 
@@ -96,52 +256,12 @@ sea_effects:
 	add x3, x3, DIST_WAVES // Espacio entre linea 
 	cbnz x9, pintarLinea 
 
-	
-//tomi barco
+br x30
 
-	movz x15, 0xFFCD , lsl 16	// color de la base del barco
-	movk x15, 0x853F , lsl 0	//
-	
-
-	movz x12, 300		// cordenada Y de 160 hasta 420
-	movz x13, 640		// 
-	mul x12, x12, x13	// calcula el centro del barco
-	add x12, x12, 200	// cordenada X de 
-	lsl x12, x12, 2		//
-	add x12,x20,x12     // 
-
-	movz x14, 0xFF , lsl 16		//color de las velas
-	movk x14, 0xFFFF , lsl 0	//
+//fin de hacer el fondo
 
 
-	bl make_boat
-
-	movz x15, 0xFF8B , lsl 16	// color de la base del barco
-	movk x15, 0x4513 , lsl 0	//	
-
-
-	movz x12, 380		// cordenada Y de 160 hasta 420
-	movz x13, 640		// 
-	mul x12, x12, x13	// calcula el centro del barco
-	add x12, x12, 360	// cordenada X de 
-	lsl x12, x12, 2		//
-	add x12,x20,x12     // 
-
-	movz x14, 0x0 , lsl 16	// color de las velas
-	movk x14, 0x0 , lsl 0	//
-
-
-	bl make_boat
-
-
-//---------------------------------------------------------------
-// Infinite Loop 
-
-InfLoop: 
-	b InfLoop
-
-
-
+// hacer el barco principio
 make_boat:
 
 
@@ -391,5 +511,5 @@ mastil2:
 
 	b back2		// al final x7 debe guardar la esquina sup derecha +1 bit
 
-
+// fin del barco
 
